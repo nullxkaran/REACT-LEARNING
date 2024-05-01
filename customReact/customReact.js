5b069a00072d67cf5cefa047d47280bd9738a1cb
nullxkaran <nullxkaran@gmail.com>
2024-05-01 12:41:55 -0700

    custome React

---
+++ customReact/customReact.js
@@ -0,0 +1,23 @@
+const container = document.querySelector("#root");
+
+// under the hood this is how react see all element passed in App.jsx.
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank',
+    },
+    children: 'click me',
+}
+
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute("target", reactElement.props.target)
+
+    container.appendChild(domElement)
+}
+
+customRender(reactElement, container)
